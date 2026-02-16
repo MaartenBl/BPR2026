122f3c2ab4b4d46792b1159554394f0103f336b6
Blömer,Maarten M.W.A. <lucmaar@hotmail.com>
2026-02-16 23:04:31 +0100

    fix: remove back button

--- src/app/componenets/puzzle1/puzzle1.ts
+++ src/app/componenets/puzzle1/puzzle1.ts
@@ -1,20 +1,19 @@
 import { Component } from '@angular/core';
-import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-puzzle1',
   standalone: true,
-  imports: [RouterLink],
+  imports: [],
   templateUrl: './puzzle1.html',
   styleUrls: ['./puzzle1.scss'],
 })
 export class Puzzle1 {
-  rebus1 = '/assets/rebus1.png';
+  rebus1 = 'assets/rebus1.png';
   locked = true;
 
   handleImageError() {
     console.error('Failed to load rebus image');
-    this.rebus1 = '/assets/placeholder.png'; // Fallback image
+    this.rebus1 = 'assets/placeholder.png'; // Fallback image
   }
 
   unlock(value: string) {
